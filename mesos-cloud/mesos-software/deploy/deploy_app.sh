d24bbf469a33128747152d4f3cccff84ea0a19e7
ctrombet <cristian.trombetta@gmail.com>
2016-02-05 11:19:35 +0000

    splitting Consul M/S

--- mesos-cloud/mesos-software/deploy/deploy_app.sh
+++ mesos-cloud/mesos-software/deploy/deploy_app.sh
@@ -2,5 +2,7 @@
 
 echo "Deploying application on Mesos Cluster"
 
-MASTER_IP=$(ec2.py --list | grep -A1 Mesos_Master | grep -A1 $(whoami) | tail -1 | awk -F\" '{print $2}')
+ENV=$1
+
+MASTER_IP=$(ec2.py --list | grep -A1 Mesos_Master | grep -A1 $ENV | grep -A1 $(whoami) | tail -1 | awk -F\" '{print $2}')
 curl -X POST http://${MASTER_IP}:8080/v2/apps -d @../applications/simple_docker_app.json -H "Content-type: application/json"
