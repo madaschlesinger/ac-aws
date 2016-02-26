5f4f56ece1ced85225f78568c679050686ff6ce1
ctrombet <cristian.trombetta@gmail.com>
2016-02-26 10:00:37 +0000

    Removing unwanted app file

--- mesos-cloud/mesos-software/deploy_master.sh
+++ mesos-cloud/mesos-software/deploy_master.sh
@@ -4,4 +4,4 @@ echo "Deploying a basic Mesos Cluster and create one environment"
 
 ENV=$1
 
-ansible-playbook -i ec2.py --extra-vars="flavour="$ENV"" --limit "tag_Name_Mesos_Master_`echo $(whoami)`_"$ENV"" playbooks/master.yml	
+ansible-playbook -i ec2.py --extra-vars="flavour="$ENV"" --limit "tag_Name_Mesos_Master_cristian_"$ENV"" playbooks/master.yml	
--- mesos-cloud/mesos-software/provision-aws.sh
+++ mesos-cloud/mesos-software/provision-aws.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-
 echo "Provisioning AWS infrastructure and building VPC, Security Groups and Hosts"
 
 ENV=$1
 
-ansible-playbook -i ec2.py --extra-vars="flavour="$ENV"" playbooks/aws-infrastructure.yml
+ansible-playbook -i ec2.py -vvvv --extra-vars="flavour="$ENV"" playbooks/aws-infrastructure.yml
