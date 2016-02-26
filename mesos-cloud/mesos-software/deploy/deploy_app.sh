fe688dbb33a466d87ebce2f2e04adfb599654808
ctrombet <cristian.trombetta@gmail.com>
2016-02-26 09:59:01 +0000

    Deploy application with HAPROXY V HOST

--- mesos-cloud/mesos-software/deploy/deploy_app.sh
+++ mesos-cloud/mesos-software/deploy/deploy_app.sh
@@ -5,4 +5,9 @@ echo "Deploying application on Mesos Cluster"
 ENV=$1
 
 MASTER_IP=$(ec2.py --list | grep -A1 Mesos_Master | grep -A1 $ENV | grep -A1 $(whoami) | tail -1 | awk -F\" '{print $2}')
-curl -X POST http://${MASTER_IP}:8080/v2/apps -d @../applications/simple_docker_app.json -H "Content-type: application/json"
+
+MASTER_ADDRESS=$(ec2.py --host ${MASTER_IP} | grep ec2_public_dns_name | awk '{print $2}' | sed 's/,//g' | sed 's/"//g')
+
+sed 's/##EXTERNAL_EC2_IP##/'"${MASTER_ADDRESS}"'/g' ../applications/nginx_haproxy.json.template >  ../applications/nginx_haproxy.json
+
+curl -X POST http://${MASTER_IP}:8080/v2/apps -d @../applications/nginx_haproxy.json -H "Content-type: application/json"
