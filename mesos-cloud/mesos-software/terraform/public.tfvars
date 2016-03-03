d81ed878e22fab1e43a0673b814700bb8482b69b
ctrombet <cristian.trombetta@gmail.com>
2016-03-03 10:05:58 +0000

    Terraform implementation of ECS cluster

---
+++ mesos-cloud/mesos-software/terraform/public.tfvars
@@ -0,0 +1,4 @@
+instance_type = "t2.micro"
+aws_key_name  = "ec2-user"
+aws_region    = "eu-west-1"
+
---
+++ mesos-cloud/mesos-software/terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_key_path" {}
+variable "aws_key_name" {}
+variable "aws_region" {}
+variable "instance_type" {}
+
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region     = "${var.aws_region}"
+}
