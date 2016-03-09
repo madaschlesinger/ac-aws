c3b7c57a5391cb2ad95f0d3ee7b119967da9ed7a
ctrombet <cristian.trombetta@gmail.com>
2016-03-09 12:19:49 +0000

    Adding a Jumphost to the deployment

---
+++ ecs/jumphost.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "jumphost" {
+    ami = "ami-8b8c57f8"
+    instance_type = "t2.micro"
+    key_name = "${var.aws_key_name}"
+    security_groups = ["${aws_security_group.jumphosts.id}"]
+    subnet_id = "${aws_subnet.jumphosts.id}"
+    associate_public_ip_address = true
+    tags {
+        Name = "JumpHost"
+    }
+}
