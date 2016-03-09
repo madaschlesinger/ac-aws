1798a0d8c28c1e3f3dd573ba0ed81f9073cd920b
ctrombet <cristian.trombetta@gmail.com>
2016-03-09 12:18:50 +0000

    Routing tables for private vpc

--- ecs/private_vpc.tf
+++ ecs/private_vpc.tf
@@ -18,6 +18,19 @@ resource "aws_route_table" "r" {
     }
 }
 
+resource "aws_route_table" "nat" {
+    vpc_id = "${aws_vpc.terraform_vpc.id}"
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = "${aws_nat_gateway.gw.id}"
+    }
+
+    tags {
+        Name = "Route Table NAT Terraform"
+    }
+}
+
+
 resource "aws_vpc" "terraform_vpc" {
     cidr_block = "10.0.0.0/16"
     tags {
@@ -37,8 +50,14 @@ resource "aws_subnet" "slaves" {
 
 resource "aws_route_table_association" "a" {
     subnet_id = "${aws_subnet.slaves.id}"
+    route_table_id = "${aws_route_table.nat.id}"
+}
+
+resource "aws_route_table_association" "jumphost-route" {
+    subnet_id = "${aws_subnet.jumphosts.id}"
     route_table_id = "${aws_route_table.r.id}"
 }
+
 
 resource "aws_subnet" "frontend" {
     vpc_id = "${aws_vpc.terraform_vpc.id}"
