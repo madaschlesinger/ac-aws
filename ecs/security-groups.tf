b70d79b64c8796033828a658acedd635c22426e6
ctrombet <cristian.trombetta@gmail.com>
2016-03-09 12:19:21 +0000

    TCP 80 security group

--- ecs/security-groups.tf
+++ ecs/security-groups.tf
@@ -24,9 +24,9 @@ resource "aws_security_group" "webservers" {
   vpc_id = "${aws_vpc.terraform_vpc.id}"
 
   ingress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
+      from_port = 80
+      to_port = 80
+      protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
   }
 
