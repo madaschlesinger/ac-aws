e36ed84aaf47237a15a1aa8cb3cdc446380c845e
ctrombet <cristian.trombetta@gmail.com>
2016-03-30 09:20:03 +0100

    Service java listening on port mapped to ELB

--- ecs/service.tf
+++ ecs/service.tf
@@ -1,21 +1,21 @@
-resource "aws_ecs_service" "nginx" {
-  name = "nginx"
+resource "aws_ecs_service" "java-cloud" {
+  name = "java-cloud"
   cluster = "${aws_ecs_cluster.jpmc-ecs-cluster.id}"
-  task_definition = "${aws_ecs_task_definition.nginx.arn}"
+  task_definition = "${aws_ecs_task_definition.java-cloud.arn}"
   desired_count = 1
   iam_role = "${aws_iam_role.terraform_ecs_instance.arn}"
 
   load_balancer {
-    elb_name = "${aws_elb.nginx.id}"
-    container_name = "nginx"
-    container_port = 80
+    elb_name = "${aws_elb.java-service.id}"
+    container_name = "johnisadaptive/services-standalone-rest"
+    container_port = 8080
   }
 
 }
 
-resource "aws_ecs_task_definition" "nginx" {
-  family = "nginx"
-  container_definitions = "${file("tasks/nginx.json")}"
+resource "aws_ecs_task_definition" "java-cloud" {
+  family = "java-cloud"
+  container_definitions = "${file("tasks/java-cloud.json")}"
 
 }
 
