54be68faa6e5ec10430401e3bd825bd55709aba9
ctrombet <cristian.trombetta@gmail.com>
2016-03-31 17:16:07 +0100

    Services Zero MQ

---
+++ ecs/elb-service-zeromq-server.tf
@@ -0,0 +1,36 @@
+# Create a new load balancer
+resource "aws_elb" "java-zeromq-server" {
+  name = "service-elb-zeromq-server"
+  subnets = ["${aws_subnet.ELBS.id}"]
+  security_groups = ["${aws_security_group.ELBS.id}"]
+
+  listener {
+    instance_port = 8083
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:8083/"
+    interval = 30
+  }
+
+  connection_draining = true
+  connection_draining_timeout = 400
+
+  tags {
+    Name = "java-service-terraform-elb"
+  }
+}
+
+resource "aws_route53_record" "zeromq-server" {
+   zone_id = "${aws_route53_zone.dev.zone_id}"
+   name = "zeromq-server"
+   type = "CNAME"
+   ttl = "300"
+   records = ["${aws_elb.java-zeromq-server.dns_name}"]
+}
