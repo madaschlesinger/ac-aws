79a65637392d02afd8b3b49f63565cf786d15a1c
ctrombet <cristian.trombetta@gmail.com>
2016-03-31 17:15:14 +0100

    Set at least 2 tasks in the indexed services

--- ecs/service-indexed-rest.tf
+++ ecs/service-indexed-rest.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_service" "services-indexed-rest" {
   name = "services-indexed-rest"
   cluster = "${aws_ecs_cluster.jpmc-ecs-cluster.id}"
   task_definition = "${aws_ecs_task_definition.services-indexed-rest.arn}"
-  desired_count = 1
+  desired_count = 2
   iam_role = "${aws_iam_role.terraform_ecs_instance.arn}"
 
   load_balancer {
