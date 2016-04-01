883f41dab77afc65556c77f2c8222d8d0eb622c2
ctrombet <cristian.trombetta@gmail.com>
2016-04-01 16:54:45 +0100

    ZeroMQ Server on port UDP 8084

--- ecs/elb-service-zeromq-server.tf
+++ ecs/elb-service-zeromq-server.tf
@@ -5,17 +5,17 @@ resource "aws_elb" "java-zeromq-server" {
   security_groups = ["${aws_security_group.ELBS.id}"]
 
   listener {
-    instance_port = 8083
-    instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
+    instance_port = 8084
+    instance_protocol = "tcp"
+    lb_port = 8084
+    lb_protocol = "tcp"
   }
 
   health_check {
     healthy_threshold = 2
     unhealthy_threshold = 2
     timeout = 3
-    target = "HTTP:8083/"
+    target = "TCP:8084"
     interval = 30
   }
 
