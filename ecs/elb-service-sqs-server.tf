0e841c0ec02be01e7dd2b2be9d244c037a4b1937
ctrombet <cristian.trombetta@gmail.com>
2016-04-04 18:36:40 +0100

    Missing ELB for SQS server

---
+++ ecs/elb-service-sqs-server.tf
@@ -0,0 +1,28 @@
+# Create a new load balancer
+resource "aws_elb" "java-sqs-server" {
+  name = "service-sqs-server"
+  subnets = ["${aws_subnet.ELBS.id}"]
+  security_groups = ["${aws_security_group.ELBS.id}"]
+
+  listener {
+    instance_port = 8087
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "HTTP:8087/"
+    interval = 30
+  }
+
+  connection_draining = true
+  connection_draining_timeout = 400
+
+  tags {
+    Name = "java-service-terraform-elb"
+  }
+}
