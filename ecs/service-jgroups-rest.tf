7c51390b08b68020323114844b0d5c4c2bfc055e
ctrombet <cristian.trombetta@gmail.com>
2016-04-04 13:02:21 +0100

    Change to one service only for JGroups

--- ecs/service-jgroups-rest.tf
+++ ecs/service-jgroups-rest.tf
@@ -2,7 +2,7 @@ resource "aws_ecs_service" "services-jgroups-rest" {
   name = "services-jgroups-rest"
   cluster = "${aws_ecs_cluster.jpmc-ecs-cluster.id}"
   task_definition = "${aws_ecs_task_definition.services-jgroups-rest.arn}"
-  desired_count = 2
+  desired_count = 1
   iam_role = "${aws_iam_role.terraform_ecs_instance.arn}"
 
   load_balancer {
