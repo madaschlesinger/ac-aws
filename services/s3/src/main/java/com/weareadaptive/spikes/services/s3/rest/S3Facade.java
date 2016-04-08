2f0a5c6b23059bbec53b2fb5b2c6cc8849b46306
Spencer <spencerartisan@gmail.com>
2016-04-08 10:12:39 +0100

    S3 sample uses profile credentials

--- services/s3/src/main/java/com/weareadaptive/spikes/services/s3/rest/S3Facade.java
+++ services/s3/src/main/java/com/weareadaptive/spikes/services/s3/rest/S3Facade.java
@@ -1,57 +1,52 @@
 package com.weareadaptive.spikes.services.s3.rest;
 
-import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
 import com.amazonaws.util.StringInputStream;
 import org.springframework.util.MultiValueMap;
 
 import java.io.*;
-import java.util.List;
 
-/**
- * Created by spencerward on 06/04/2016.
- */
-public class S3Facade {
+class S3Facade {
     private final AmazonS3Client s3client;
-    private String bucketName;
+    private final String bucketName;
 
-    public S3Facade(String bucketName) {
+    S3Facade(String bucketName) {
         this.bucketName = bucketName;
-        s3client = new AmazonS3Client(new EnvironmentVariableCredentialsProvider());
-        Region region = Region.getRegion(Regions.EU_WEST_1);
-        s3client.setRegion(region);
+        s3client = new AmazonS3Client(new InstanceProfileCredentialsProvider());
+        s3client.setRegion(Region.getRegion(Regions.EU_WEST_1));
     }
 
-    public String getData(String path) {
+    void setData(String path, MultiValueMap parameters) {
         try {
-            if (!s3client.doesObjectExist(bucketName, path)) {
-                return "No data here";
-            }
+            InputStream data = new StringInputStream(parameters.toString());
+            s3client.putObject(new PutObjectRequest(bucketName, path, data, new ObjectMetadata()));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-            S3Object data = s3client.getObject(new GetObjectRequest(bucketName, path));
-            BufferedReader reader = new BufferedReader(new InputStreamReader(data.getObjectContent()));
+    String getData(String path) {
+        if (!s3client.doesObjectExist(bucketName, path)) {
+            return "No data here";
+        }
+
+        S3Object data = s3client.getObject(new GetObjectRequest(bucketName, path));
+        return readStream(data.getObjectContent());
+    }
+
+    private String readStream(InputStream stream) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream));) {
             StringBuilder out = new StringBuilder();
             String line;
             while ((line = reader.readLine()) != null) {
                 out.append(line);
             }
-            reader.close();
             return out.toString();
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void setData(String path, MultiValueMap parameters) {
-        try {
-            InputStream data = new StringInputStream(parameters.toString());
-            ObjectMetadata metaData = new ObjectMetadata();
-            s3client.putObject(new PutObjectRequest(bucketName, path, data, metaData));
-        } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
     }
