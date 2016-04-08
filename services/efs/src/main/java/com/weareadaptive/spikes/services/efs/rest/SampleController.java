62391da158e6cf2db3c5e47e37000f03ffae06d8
Spencer <spencerartisan@gmail.com>
2016-04-08 09:46:37 +0100

    Skeletal erf rest service

---
+++ services/efs/src/main/java/com/weareadaptive/spikes/services/efs/rest/SampleController.java
@@ -0,0 +1,48 @@
+package com.weareadaptive.spikes.services.efs.rest;
+
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.HandlerMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+    private static final String BUCKET_NAME_ENV = "S3_BUCKET";
+
+    private S3Facade s3Facade;
+
+    public SampleController() throws Exception {
+        String bucketName = System.getenv(BUCKET_NAME_ENV);
+        s3Facade = new S3Facade(bucketName);
+    }
+
+    @RequestMapping("/**")
+    @ResponseBody
+    String home(@RequestParam MultiValueMap parameters, HttpServletRequest request) {
+        String path = request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString().substring(1);
+        if (!parameters.isEmpty()) {
+            s3Facade.setData(path, parameters);
+        }
+        return s3Facade.getData(path);
+    }
+
+    @RequestMapping("/health")
+    @ResponseBody
+    String health() {
+        return "healthy";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleController.class, args);
+    }
+}
