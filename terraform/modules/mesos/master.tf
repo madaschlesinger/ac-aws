124619db1c135b179b63848156a1dabd35cfe340
ctrombet <cristian.trombetta@gmail.com>
2016-04-19 16:32:20 +0100

    Saving intermediate solution with elastic Network Interface

--- terraform/modules/mesos/master.tf
+++ terraform/modules/mesos/master.tf
@@ -1,12 +1,24 @@
+## First create a stable point to call master
+
+resource "aws_network_interface" "mesos-master" {
+    subnet_id = "${aws_subnet.master.id}"
+    private_ips = ["10.1.7.50"]
+    security_groups = ["${aws_security_group.master.id}"]
+    source_dest_check = "false"
+    attachment {
+        instance = "${aws_instance.master.id}"
+        device_index = 1
+    }
+}
+
 resource "aws_instance" "master" {
-    ami = "ami-d4fc71a7"
+    ami = "ami-d7d35fa4"
     instance_type = "t2.micro"
     key_name = "${var.aws_key_name}"
     security_groups = ["${aws_security_group.master.id}"]
     subnet_id = "${aws_subnet.master.id}"
     associate_public_ip_address = "true"
     source_dest_check = "false"
-    user_data = "#!/bin/bash\nexport EXTERNAL_IP='${public_ip}' && /tmp/user_data.sh"
     tags {
         Name = "Mesos Master"
     }
@@ -15,6 +27,10 @@ resource "aws_instance" "master" {
 resource "aws_route_table_association" "master-routing" {
     subnet_id = "${aws_subnet.master.id}"
     route_table_id = "${aws_route_table.r.id}"
+}
+
+output "mesos-console" {
+    value = "http://${aws_instance.master.public_dns}:5050"
 }
 
 
