60980caf223f1b60ddad61c7166caa407c112f14
ctrombet <cristian.trombetta@gmail.com>
2016-04-22 09:28:12 +0100

    Script to configure Master with Cloud Init

--- terraform/modules/mesos/packer/scripts/master-config.sh
+++ terraform/modules/mesos/packer/scripts/master-config.sh
@@ -1,4 +1,3 @@
-#cloud-boothook
 #!/bin/bash
 
 EXTERNAL_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
@@ -6,3 +5,4 @@ EXTERNAL_IP=`curl http://169.254.169.254/latest/meta-data/public-ipv4`
 sed -i 's/EXTERNAL_IP/'"${EXTERNAL_IP}"'/g' /etc/marathon/conf/hostname
 sed -i 's/HOSTNAME/'"${HOSTNAME}"'/g' /usr/lib/systemd/system/consul.service
 sed -i 's/HOSTNAME/'"${HOSTNAME}"'/g' /etc/zookeeper/conf/zoo.cfg
+systemctl restart marathon
