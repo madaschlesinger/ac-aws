6a7c9b896905865aaafb3ae382eff968d453daaa
ctrombet <cristian.trombetta@gmail.com>
2016-04-22 14:55:30 +0100

    Extra subnet - Mesos Public slaves

--- terraform/modules/mesos/public_vpc.tf
+++ terraform/modules/mesos/public_vpc.tf
@@ -13,3 +13,22 @@ resource "aws_route_table_association" "nat-route-table" {
     subnet_id = "${aws_subnet.natgateways.id}"
     route_table_id = "${aws_route_table.r.id}"
 }
+
+## Create Public Network for Public Mesos slaves
+
+resource "aws_subnet" "mesos_slaves_public" {
+    vpc_id = "${aws_vpc.mesos.id}"
+    availability_zone = "eu-west-1a"
+    cidr_block = "10.1.8.0/24"
+
+    tags {
+        Name = "Terraform_public_mesos_slaves"
+    }
+}
+
+resource "aws_route_table_association" "nat-route-table-public" {
+    subnet_id = "${aws_subnet.mesos_slaves_public.id}"
+    route_table_id = "${aws_route_table.r.id}"
+}
+
+
