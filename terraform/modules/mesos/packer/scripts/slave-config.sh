942f9dad4e57e3e5537bf00f8fa194e0ecf606f4
ctrombet <cristian.trombetta@gmail.com>
2016-04-22 09:28:25 +0100

    Script to configure Slave with Cloud Init

--- terraform/modules/mesos/packer/scripts/slave-config.sh
+++ terraform/modules/mesos/packer/scripts/slave-config.sh
@@ -4,13 +4,13 @@ MASTER=`curl http://169.254.169.254/latest/user-data | awk -F= '{print $2}'`
 IP=`ifconfig eth0 | grep inet | head -1 | awk '{print $2}'`
 
 sed -i 's/MASTER/'"${MASTER}"'/g' /etc/mesos/zk
-sed -i 's/MASTER/'"${MASTER}"'/g' /usr/lib/systemd/system/weave.service
-sed -i 's/MASTER/'"${MASTER}"'/g' /usr/lib/systemd/system/consul-slave.service
+#sed -i 's/MASTER/'"${MASTER}"'/g' /usr/lib/systemd/system/weave.service
+#sed -i 's/MASTER/'"${MASTER}"'/g' /usr/lib/systemd/system/consul-slave.service
 
 
 sed -i 's/EXTERNAL_IP/'"${HOSTNAME}"'/g' /etc/mesos-slave/hostname
 sed -i 's/HOSTNAME/'"${IP}"'/g' /etc/mesos-slave/ip
 
 systemctl restart mesos-slave
-systemctl restart weave 
-systemctl restart consul-slave
+#systemctl restart weave 
+#systemctl restart consul-slave
