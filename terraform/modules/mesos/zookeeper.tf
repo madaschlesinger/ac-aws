0427170d6bb2c79384081be08a166fe85e5ebf29
ctrombet <cristian.trombetta@gmail.com>
2016-04-28 11:12:34 +0100

    Setup ELB for Zookeeeper

--- terraform/modules/mesos/zookeeper.tf
+++ terraform/modules/mesos/zookeeper.tf
@@ -23,3 +23,34 @@ resource "aws_route_table_association" "zookeeper-routing" {
     subnet_id = "${aws_subnet.shared-services.id}"
     route_table_id = "${aws_route_table.r.id}"
 }
+
+## First create a stable point to call Zookeeper 
+
+resource "aws_elb" "zookeeper-elb" {
+  name = "zookeeper-elb"
+  internal = "true"
+  subnets = ["${aws_subnet.shared-services.id}"]
+  security_groups = ["${aws_security_group.shared-services.id}"]
+
+  listener {
+    instance_port = 2181
+    instance_protocol = "tcp"
+    lb_port = 2181
+    lb_protocol = "tcp"
+  }
+
+  health_check {
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+    timeout = 3
+    target = "TCP:2181"
+    interval = 30
+  }
+  instances = ["${aws_instance.zookeeper.id}"]
+  connection_draining = true
+  connection_draining_timeout = 400
+
+  tags {
+    Name = "zookeeper-elb"
+  }
+}
