bd460fdad54c996ab979eb905af21dad75a866ed
ctrombet <cristian.trombetta@gmail.com>
2016-04-28 11:49:12 +0100

    Adding tag Group

--- terraform/provisioning/standalone-ecs/test-modules.tf
+++ terraform/provisioning/standalone-ecs/test-modules.tf
@@ -3,6 +3,7 @@ variable "aws_secret_key" {}
 variable "aws_key_name" {}
 variable "instance_type" {}
 variable "aws_region" {}
+variable "tag_value" {}
 
 provider "aws" {
     access_key = "${var.aws_access_key}"
@@ -19,5 +20,6 @@ module "ecs" {
     aws_key_name   = "ec2-user"
     instance_type  = "t2.micro"
     sec_groups_ecs = "10.0.1.0/16"
+    tag_value      = "${var.tag_value}"
     
 }
