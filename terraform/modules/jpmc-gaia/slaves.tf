f214d7431e7fcca49cd78b9581f70afcbce3ff4e
ctrombet <cristian.trombetta@gmail.com>
2016-05-31 13:52:43 +0100

    MEsos Slaves autoscaling group

--- terraform/modules/jpmc-gaia/slaves.tf
+++ terraform/modules/jpmc-gaia/slaves.tf
@@ -1,15 +1,27 @@
-resource "aws_instance" "slave" {
-    ami = "${var.slaves_ami}"
-    instance_type = "t2.micro"
-    key_name = "${var.aws_key_name}"
-    security_groups = ["${aws_security_group.slaves.id}"]
-    subnet_id = "${var.aws_subnet_mesos_slaves}"
-    associate_public_ip_address = "false"
-    user_data = "ZOOKEEPER=${aws_instance.zookeeper.private_ip}\nMASTER=${aws_instance.master.private_ip}"
-    source_dest_check = "false"
-    tags {
-        Name = "Mesos Slave"
-        Group = "${var.tag_value}"
-    }
+resource "aws_autoscaling_group" "terraform-mesos-cluster" {
+    availability_zones = ["${var.AZ}"]
+    name = "jpmc-mesos-cluster"
+    min_size = 1
+    max_size = 3
+    desired_capacity = 1
+    health_check_type = "EC2"
+    launch_configuration = "${aws_launch_configuration.mesos-slaves.name}"
+    vpc_zone_identifier = ["${var.aws_subnet_mesos_slaves}"]
+    tag {
+          key = "Name"
+          value = "Mesos Slave Cluster"
+          propagate_at_launch = true
+        }
 }
 
+
+resource "aws_launch_configuration" "mesos-slaves" {
+    image_id = "${var.slaves_ami}"
+    instance_type = "${var.instance_type}"
+    security_groups = ["${var.security_group}"]
+    iam_instance_profile = "JPMC_AppServices_S3_Bucket_Access"
+    key_name = "${var.aws_key_name}"
+    user_data = "ZOOKEEPER=${aws_instance.zookeeper.private_ip}\nMASTER=${aws_instance.master.private_ip}\nYUM_REPO_IP=${aws_instance.yum.private_ip}"
+    associate_public_ip_address = false
+
+}
