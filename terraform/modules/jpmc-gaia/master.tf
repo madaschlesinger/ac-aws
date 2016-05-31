64ce153ac4296e62673099b9821fe8db71c70939
ctrombet <cristian.trombetta@gmail.com>
2016-05-31 13:51:24 +0100

    Parameters for SGs and add YUM repository configuration

--- terraform/modules/jpmc-gaia/master.tf
+++ terraform/modules/jpmc-gaia/master.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "master" {
     ami = "${var.master_ami}"
     instance_type = "t2.micro"
     key_name = "${var.aws_key_name}"
-    security_groups = ["${aws_security_group.master.id}"]
+    security_groups = ["${var.security_group}"]
     subnet_id = "${var.aws_subnet_master}"
     associate_public_ip_address = "false"
-    user_data = "ZOOKEEPER=${aws_instance.zookeeper.private_ip}"
+    user_data = "ZOOKEEPER=${aws_instance.zookeeper.private_ip}\nYUM_REPO_IP=${aws_instance.yum.private_ip}"
     source_dest_check = "false"
     tags {
         Name = "Mesos Master"
